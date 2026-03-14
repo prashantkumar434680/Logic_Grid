0fff7eafb3048b2bf9e5ea652f1c689a8c21d50f
Prashant <prashantjat622@gmail.com>
2026-03-14 11:12:42 +0530

    8th commit

--- Backend/src/Routes/submit.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userMiddleware = require("../middleware/userMiddleware");
-const submitRouter = express.Router();
-
-const submitCode = submitRouter.post("/submit/:id",userMiddleware, submitCode);
-const runCode = submitRouter.post("/run/:id",userMiddleware, runCode);
-
-module.exports = {submitCode,runCode};
---
+++ back/src/Routes/ProblemCreator.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const problemRouter =  express.Router();
+const adminMiddleware = require("../middleware/adminMiddleware");
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblembyUser,submittedProblem} = require("../controllers/userProblem");
+const userMiddleware = require("../middleware/userMiddleware");
+
+
+// Create
+problemRouter.post("/create" ,adminMiddleware,createProblem);
+problemRouter.put("/update/:id",adminMiddleware, updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware, deleteProblem);
+problemRouter.get("/test",adminMiddleware, (req,res)=>{
+    res.status(200).send("Admin Middleware is working");
+});
+
+problemRouter.get("/problemById/:id",userMiddleware,getProblemById);
+problemRouter.get("/getAllProblem",userMiddleware, getAllProblem);
+problemRouter.get("/problemSolvedByUser",userMiddleware, solvedAllProblembyUser);
+problemRouter.get('/submittedProblem/:pid',userMiddleware,submittedProblem);
+
+
+module.exports = problemRouter;
+
